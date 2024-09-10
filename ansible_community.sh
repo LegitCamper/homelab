0c4bc0a505efc7948c8931f464deb013b047d90e
sawyer bristol <sawyerbristol@gmail.com>
2024-09-10 16:11:44 -0600

    fixing issues

---
+++ ansible_community.sh
@@ -0,0 +1,4 @@
+ansible-galaxy collection install community.docker
+ansible-galaxy install jedimt.nvidia_container_toolkit
+ansible-galaxy role install artis3n.tailscale
+ansible-galaxy install -r requirements.yml --force
