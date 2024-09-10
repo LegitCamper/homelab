983e86d0b6ec1094ed73b55f49965765dba33215
sawyer bristol <sawyerbristol@gmail.com>
2024-09-10 16:57:04 -0600

    use requirements.yml file instead of bash script

--- ansible_community.sh
+++
@@ -1,4 +0,0 @@
-ansible-galaxy collection install community.docker
-ansible-galaxy install jedimt.nvidia_container_toolkit
-ansible-galaxy role install artis3n.tailscale
-ansible-galaxy install -r requirements.yml --force
